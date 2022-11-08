612f2da3c92a1877a700fcb1a170cab257e696e2
lovroselic <lovro.selic@gmail.com>
2022-11-08 16:05:49 +0100

    0.13.00

--- RUN.js
+++ RUN.js
@@ -47,7 +47,7 @@ var INI = {
     AIR_COST: 1,
 };
 var PRG = {
-    VERSION: "0.12.19",
+    VERSION: "0.13.00",
     NAME: "R.U.N.",
     YEAR: "2022",
     CSS: "color: #239AFF;",
@@ -609,7 +609,7 @@ var GAME = {
         GAME.won = false;
         //GAME.level = 1;
         //GAME.level = 6;
-        //GAME.level = 3;
+        //GAME.level = 2;
         GAME.level = 3;
         GAME.score = 0;
         GAME.lives = 3;
