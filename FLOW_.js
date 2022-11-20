868b9864ab79592debc2dc4bf096282b2c72f9a7
lovroselic <lovro.selic@gmail.com>
2022-11-20 17:44:12 +0100

    0.15.01

--- FLOW_.js
+++ FLOW_.js
@@ -45,7 +45,7 @@ class Boundaries {
     }
 }
 var FLOW = {
-    VERSION: "III.1.0",
+    VERSION: "III.1.1",
     CSS: "color: #F3A",
     DEBUG: true,
     PAINT_DISTANCES: true,
@@ -58,18 +58,14 @@ var FLOW = {
     },
     layer: 'flood',
     map: null,
-    //sizeMap: null,
     GA: null,
     NA: null,
-    //flood_level: Infinity,
     init(map, origin) {
         this.map = map;
         this.GA = map.GA;
         this.origin = origin;
         this.origin_level = this.origin.y;
         this.origin_index = this.GA.gridToIndex(this.origin);
-        //this.sizeMap = new Float32Array(this.map.width * this.map.height);
-        //this.sizeMap[this.origin_index] = FLOW.INI.ORIGIN_SIZE;
         this.make_NA();
         this.NA.G_set(this.origin, 'size', FLOW.INI.ORIGIN_SIZE);
         console.log(`%cFLOW initialized`, FLOW.CSS, FLOW);
@@ -124,7 +120,10 @@ var FLOW = {
         let DRAIN_DEBT = [];
         let count = 0;
         let distance = 0;
-        let flow = this.INI.ORIGIN_FLOW;
+        //let flow = this.INI.ORIGIN_FLOW;
+        const drain_update = (lapsedTime / 1000 * ((FLOW.INI.ORIGIN_FLOW * FLOW.INI.DRAIN_FACTOR) / ENGINE.INI.GRIDPIX ** 2));
+        const initial_flow = (lapsedTime / 1000 * (this.INI.ORIGIN_FLOW / ENGINE.INI.GRIDPIX ** 2));
+        let flow = initial_flow;
         let PATH = new Int8Array(this.map.width * this.map.height);
         while (STACK.length > 0) {
             count++; // DEBUG
@@ -132,6 +131,14 @@ var FLOW = {
             if (FLOW.DEBUG) console.log("iteraton:", count, "distance:", distance, "flow:", flow);
             let LINE = STACK.pop();
             if (FLOW.DEBUG) console.log("LINE:", LINE);
+
+            //if (count >= Infinity) {
+            if (count >= 7) {
+                console.warn("terminated for safety & debug");
+                throw "DEBUG";
+                //break;
+            }
+
             DRAIN_DEBT.removeIfInArray(LINE);
             if (FLOW.DEBUG) console.log("DRAIN_DEBT before:", ...DRAIN_DEBT);
 
@@ -139,39 +146,69 @@ var FLOW = {
             let line_above = this.get_line_above(LINE, marks);
             if (FLOW.DEBUG) console.log("line_above:", line_above);
 
-            let borrowed_drain = this.borrow_drain(lapsedTime, line_above, DRAIN_DEBT) * this.INI.ORIGIN_FLOW;
-            flow += borrowed_drain;
-            if (FLOW.DEBUG) console.log("borrowed_drain:", borrowed_drain, "flow:", flow);
-            if (FLOW.DEBUG) console.log("DRAIN_DEBT after:", ...DRAIN_DEBT);
+            if (line_above.length > 0) {
+                let borrowed_drain = this.borrow_drain(drain_update, line_above, DRAIN_DEBT);
+                flow += borrowed_drain;
+                if (FLOW.DEBUG) console.log("borrowed_drain:", borrowed_drain, "flow:", flow);
+                if (FLOW.DEBUG) console.log("DRAIN_DEBT after:", ...DRAIN_DEBT);
+            }
 
-            /*
-                * check for down candidates: *down*
-                * yes:
-                    * put *down* candidates on stack
-                    * continue
-            */
-            let [line_below, drain_on_path] = this.line_below(lapsedTime, LINE, DRAIN_DEBT, PATH);
+            let [line_below, drain_on_path] = this.line_below(drain_update, LINE, DRAIN_DEBT, PATH, marks);
             if (FLOW.DEBUG) console.log("line_below:", line_below, "drain_on_path:", drain_on_path);
             if (FLOW.DEBUG) console.log("DRAIN_DEBT after flowing down:", ...DRAIN_DEBT);
+            flow += drain_on_path;
 
             if (line_below) {
+                //line_below = this.extend_line(line_below, marks);
+                //if (FLOW.DEBUG) console.log("line_below after extension:", line_below);
                 if (FLOW.DEBUG) console.log("# Flow goes on: ", line_below);
                 STACK.push(line_below);
+                if (FLOW.DEBUG) console.log("\n-------------------");
                 continue;
             }
+            
+
+            /*
+                * (implicit no);
+                * line ready to be processed; (what if meeting of drains and terminals)
+                    * line has different levels:
+                        * calc avg,min,max of *line*: *avg_line*, *min_line*, *max_line* (of fulness!)
+                * apply total flow (flow+drain +excess) - to min levels
+                * check min/max diff < E :
+                    * set all to (min+max)/2
+
+            */
+
+            let [MIN, MAX] = this.analyze(LINE, "full");
+            if (FLOW.DEBUG) console.log("LINE analysis", MIN, MAX);
+            let min_line = this.filter_min_line(LINE, MIN);
+            if (FLOW.DEBUG) console.log("min_line", min_line);
+            if (FLOW.DEBUG) console.log("current FLOW", flow);
+
+
+            this.apply_flow(min_line, distance, PATH, flow);
+            distance++;
+
+            /**
+             * check min/max diff < E
+             * not yet implemented
+             */
 
 
             //
 
+            let excess_flow = this.overflow(LINE);
+            flow = excess_flow;
+            if (FLOW.DEBUG) console.log("excess FLOW", excess_flow);
+
+            if (line_above.length > 0 && flow > 0) {
+                STACK.push(line_above);
+            }
+
+
             //end while
             if (FLOW.DEBUG) console.log("\n-------------------");
-            if (count > 10) {
-                console.warn("terminated for safety & debug");
-                break;
-            }
         }
-
-
 
         if (FLOW.DEBUG) {
             console.log("********************************");
@@ -179,7 +216,56 @@ var FLOW = {
         }
         console.timeEnd("Flow");
     },
-    line_below(lapsedTime, line, DRAIN_DEBT, PATH) {
+    overflow(line) {
+        let excess_flow = 0;
+        for (let node of line) {
+            let NODE = this.NA.map[node];
+            if (NODE.size > NODE.max_flow) {
+                excess_flow += NODE.size - NODE.max_flow;
+                NODE.size = NODE.max_flow;
+                NODE.refresh();
+            }
+            if (FLOW.DEBUG) console.log("... NODE after overflow", node, "size", NODE.size, "fullness", NODE.full);
+        }
+
+        return excess_flow;
+    },
+    apply_flow(line, distance, PATH, flow) {
+        //const flow_update = (lapsedTime / 1000 * (flow / ENGINE.INI.GRIDPIX ** 2));
+        let F = flow / line.length;
+        if (FLOW.DEBUG) console.log("..Applying flow ", flow, "per node:", F);
+        for (let node of line) {
+            PATH[node] = 1;
+            let NODE = this.NA.map[node];
+            NODE.distance = distance;
+            NODE.size += F;
+            NODE.refresh();
+            if (FLOW.DEBUG) console.log("... NODE", node, "size", NODE.size, "fullness", NODE.full);
+        }
+    },
+    filter_min_line(line, min) {
+        let min_line = [];
+        for (let node of line) {
+            if (this.NA.map[node].full === min) {
+                min_line.push(node);
+            }
+        }
+        return min_line;
+    },
+    drain_node(drain_update, DRAIN_DEBT, node) {
+        let NODE = this.NA.map[node];
+        let borrow = 0;
+        if (NODE.size > 0) {
+            borrow = drain_update * NODE.max_flow;
+            NODE.size -= borrow;
+            NODE.refresh();
+            DRAIN_DEBT.push(node);
+            if (FLOW.DEBUG) console.log("... draining node", node, "size", NODE.size, "full", NODE.full);
+        }
+        return borrow;
+
+    },
+    line_below(drain_update, line, DRAIN_DEBT, PATH, marks) {
         const DY = this.map.width;
         let below_line = [];
         for (let node of line) {
@@ -191,7 +277,6 @@ var FLOW = {
         if (below_line.length === 0) return [null, 0];
 
         let drain = 0;
-        const drain_update = (lapsedTime / 1000 * ((-FLOW.INI.ORIGIN_FLOW * FLOW.INI.DRAIN_FACTOR) / ENGINE.INI.GRIDPIX ** 2));
         let dug = [];
 
         for (let d of below_line) {
@@ -202,52 +287,42 @@ var FLOW = {
                     add = false;
                     break;
                 }
-
                 PATH[d] = 1;
-
-                let NODE = this.NA.map[d];
-                if (NODE.size > 0) {
-                    let borrow = drain_update * NODE.max_flow;
-                    NODE.size -= borrow;
-                    drain += borrow;
-                    DRAIN_DEBT.push(node);
-                }
-
+                drain += this.drain_node(drain_update, DRAIN_DEBT, d);
                 p = d;
                 d += DY;
             }
             if (add) dug.push(p);
         }
+        dug = this.extend_line(dug, marks);
+        if (FLOW.DEBUG) console.log("...dug before extension", ...dug);
+        for (let d of dug) {
+            if (!PATH[d]) {
+                PATH[d] = 1;
+                drain += this.drain_node(drain_update, DRAIN_DEBT, d);
+            }
+        }
+        if (FLOW.DEBUG) console.log("...dug after extension", ...dug);
         return [dug, drain];
     },
-    free_below() { },
     analyze(node_list, property) {
         let min = Infinity;
         let max = -Infinity;
         for (let node of node_list) {
             let NODE = this.NA.map[node];
             if (NODE[property] > max) max = NODE[property];
-            else if (NODE[property] < min) min = NODE[property];
+            if (NODE[property] < min) min = NODE[property];
         }
         return [min, max];
     },
-    borrow_drain(lapsedTime, line_above, DRAIN_DEBT) {
+    borrow_drain(drain_update, line_above, DRAIN_DEBT) {
         let [min, max] = this.analyze(line_above, 'full');
         console.log("..Borrow: analysis", min, max);
         if (max === 0) return 0;
         let drain = 0;
-        const drain_update = (lapsedTime / 1000 * ((-FLOW.INI.ORIGIN_FLOW * FLOW.INI.DRAIN_FACTOR) / ENGINE.INI.GRIDPIX ** 2));
         if (FLOW.DEBUG) console.log("..Borrowing drain", drain_update);
         for (let node of line_above) {
-
-            let NODE = this.NA.map[node];
-            if (NODE.size > 0) {
-                let borrow = drain_update * NODE.max_flow;
-                NODE.size -= borrow;
-                drain += borrow;
-                DRAIN_DEBT.push(node);
-            }
-
+            drain += this.drain_node(drain_update, DRAIN_DEBT, node);
         }
         return drain;
     },
@@ -255,6 +330,7 @@ var FLOW = {
         const DY = this.map.width;
         let line_above = [];
         for (let node of line) {
+            if (this.GA.icheck(node, MAPDICT.TRAP_DOOR)) continue;
             const above = node - DY;
             if (this.NA.map[above]) {
                 if (marks[above] === 0) {
@@ -265,6 +341,13 @@ var FLOW = {
             }
         }
         return line_above;
+    },
+    extend_line(line, marks) {
+        let extended_line = [];
+        for (let node of line) {
+            extended_line = extended_line.concat(this.next_line(node, marks));
+        }
+        return extended_line;
     },
     next_line(node, marks) {
         marks[node] = 1;
